6cb279bfe00b68cb0d8f66cba5687bf363a13554
GiftXXVI <gift.chimphonda@outlook.com>
2021-11-17 05:12:52 +0200

    Added Branch Details

--- boarderectors-client/src/App.js
+++ boarderectors-client/src/App.js
@@ -8,14 +8,31 @@ function App() {
   React.useEffect(() => {
     fetch("/agents/ACC001")
       .then((res) => res.json())
-      .then((data) => setData(data.companyName));
+      .then((data) => setData(data));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{!data ? "Loading..." : "Account Code: " + data.accountCode}</p>
+        <p>{!data ? "Loading..." : "Company Name: " + data.companyName}</p>
+        <p>
+          {!data
+            ? "Loading..."
+            : "Address: House #" +
+              data.branchAddress.houseNumber +
+              "," +
+              data.branchAddress.address1 +
+              "," +
+              data.branchAddress.locality +
+              "," +
+              data.branchAddress.town +
+              "," +
+              data.branchAddress.county}
+        </p>
+        <p>
+          {!data ? "Loading..." : "PostCode: " + data.branchAddress.postcode}
+        </p>
       </header>
     </div>
   );
