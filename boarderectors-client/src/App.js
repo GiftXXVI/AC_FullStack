0b85a9e6cbf21d55d2986b90e9583203960d3af9
GiftXXVI <gift.chimphonda@outlook.com>
2021-11-17 07:46:04 +0200

    Started Properties

--- boarderectors-client/src/App.js
+++ boarderectors-client/src/App.js
@@ -3,36 +3,75 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [agent, setAgent] = React.useState(null);
+  const [properties, setProperties] = React.useState([]);
 
   React.useEffect(() => {
     fetch("/agents/ACC001")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((agent) => setAgent(agent));
+  }, []);
+
+  React.useEffect(() => {
+    fetch("/agents/ACC001/properties")
+      .then((res) => res.json())
+      .then((properties) => {
+        setProperties(properties);
+      });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{!data ? "Loading..." : "Account Code: " + data.accountCode}</p>
-        <p>{!data ? "Loading..." : "Company Name: " + data.companyName}</p>
+        <p>{!agent ? "Loading..." : "Account Code: " + agent.accountCode}</p>
+        <p>{!agent ? "Loading..." : "Company Name: " + agent.companyName}</p>
         <p>
-          {!data
+          {!agent
             ? "Loading..."
-            : "Address: House #" +
-              data.branchAddress.houseNumber +
+            : "Address:" +
+              agent.branchAddress.houseNumber +
+              " " +
+              agent.branchAddress.address1 +
               "," +
-              data.branchAddress.address1 +
+              agent.branchAddress.locality +
               "," +
-              data.branchAddress.locality +
+              agent.branchAddress.town +
               "," +
-              data.branchAddress.town +
+              agent.branchAddress.county +
               "," +
-              data.branchAddress.county}
+              agent.branchAddress.postcode}
         </p>
-        <p>
-          {!data ? "Loading..." : "PostCode: " + data.branchAddress.postcode}
-        </p>
+        <table>
+          <thead>
+            <tr>
+              <th>Address</th>
+              <th>Erected Board Type</th>
+              <th>Fee Charged</th>
+            </tr>
+          </thead>
+          <tbody>
+            {properties.map((prop) => (
+              <tr>
+                <td>
+                  {prop.address.houseNumber +
+                    " " +
+                    prop.address.address1 +
+                    ", "}
+                  <br />
+                  {prop.address.locality + ", "}
+                  <br />
+                  {prop.address.town + ", "}
+                  <br />
+                  {prop.address.county + ", "}
+                  <br />
+                  {prop.address.postcode}
+                </td>
+                <td>{prop.id}</td>
+                <td>{prop.id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
