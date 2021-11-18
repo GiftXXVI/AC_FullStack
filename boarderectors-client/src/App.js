879529284e228016a54abe63ab961efa3d5f08ff
Gift Chimphonda <gift.chimphonda@outlook.com>
2021-11-18 14:29:36 +0200

    Added If Else Logic for Errors

--- boarderectors-client/src/App.js
+++ boarderectors-client/src/App.js
@@ -38,8 +38,8 @@ function App() {
       });
   }, []);
 
-  if (agenterr || properr) {
-    return(
+  if (agenterr) {
+    return (
       <Container>
         An Error Occured
         <div class="jumbotron">
@@ -55,7 +55,8 @@ function App() {
         </div>
       </Container>
     )
-  } else {
+  }
+  else if (!agenterr) {
     return (
       <Container>
         <div class="jumbotron">
@@ -78,69 +79,73 @@ function App() {
             {!agent ? "Loading..." : agent.branchAddress.postcode} <br />
           </p>
         </div>
-        <Container>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Address</th>
-                <th>Board Type Title</th>
-                <th>Fee Charged</th>
-              </tr>
-            </thead>
-            <tbody>
-              {properties.map((prop) => (
+
+        {!properr ?
+          <Container>
+            <Table striped bordered hover>
+              <thead>
                 <tr>
-                  <td>
-                    {prop.address.houseNumber +
-                      " " +
-                      prop.address.address1 +
-                      ", "}
-                    <br />
-                    {prop.address.locality + ", "}
-                    <br />
-                    {prop.address.town + ", "}
-                    <br />
-                    {prop.address.county + ", "}
-                    <br />
-                    {prop.address.postcode}
+                  <th>Address</th>
+                  <th>Board Type Title</th>
+                  <th>Fee Charged</th>
+                </tr>
+              </thead>
+              <tbody>
+                {properties.map((prop) => (
+                  <tr>
+                    <td>
+                      {prop.address.houseNumber +
+                        " " +
+                        prop.address.address1 +
+                        ", "}
+                      <br />
+                      {prop.address.locality + ", "}
+                      <br />
+                      {prop.address.town + ", "}
+                      <br />
+                      {prop.address.county + ", "}
+                      <br />
+                      {prop.address.postcode}
+                    </td>
+                    <td>{prop.erectedBoardType.title}</td>
+                    <td>
+                      {prop.erectedBoardType.title === "Sold"
+                        ? Math.round(
+                          (prop.totalFeeCharged +
+                            (prop.totalFeeCharged * 7.5) / 100 +
+                            Number.EPSILON) *
+                          100
+                        ) / 100
+                        : Math.round(
+                          (prop.totalFeeCharged +
+                            (prop.totalFeeCharged * 4) / 100 +
+                            Number.EPSILON) *
+                          100
+                        ) / 100}
+                    </td>
+                  </tr>
+                ))}
+                <tr>
+                  <td colSpan="2">
+                    <b>Grand Total</b>
                   </td>
-                  <td>{prop.erectedBoardType.title}</td>
                   <td>
-                    {prop.erectedBoardType.title === "Sold"
-                      ? Math.round(
-                        (prop.totalFeeCharged +
-                          (prop.totalFeeCharged * 7.5) / 100 +
-                          Number.EPSILON) *
-                        100
-                      ) / 100
-                      : Math.round(
-                        (prop.totalFeeCharged +
-                          (prop.totalFeeCharged * 4) / 100 +
-                          Number.EPSILON) *
-                        100
-                      ) / 100}
+                    {properties.reduce((total, current) => {
+                      var val = parseFloat(current.totalFeeCharged);
+                      if (current.erectedBoardType.title === "Sold") {
+                        total = total + val + (val * 7.5) / 100;
+                      } else {
+                        total = total + val + (val * 4) / 100;
+                      }
+                      return Math.round((total + Number.EPSILON) * 100) / 100;
+                    }, 0)}
                   </td>
                 </tr>
-              ))}
-              <tr>
-                <td colSpan="2">
-                  <b>Grand Total</b>
-                </td>
-                <td>
-                  {properties.reduce((total, current) => {
-                    var val = parseFloat(current.totalFeeCharged);
-                    if (current.erectedBoardType.title === "Sold") {
-                      total = total + val + (val * 7.5) / 100;
-                    } else {
-                      total = total + val + (val * 4) / 100;
-                    }
-                    return Math.round((total + Number.EPSILON) * 100) / 100;
-                  }, 0)}
-                </td>
-              </tr>
-            </tbody>
-          </Table>
-        </Container>
+              </tbody>
+            </Table>
+          </Container>
+          : ""
+        }
       </Container>
     );
   }
